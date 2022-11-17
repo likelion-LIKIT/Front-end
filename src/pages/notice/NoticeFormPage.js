dfcd54cd09aad7053f411363d1299ed50d8c799f
GuHyeonWoo <ghw9174@gmail.com>
2022-11-18 00:28:02 +0900

    feat: 파일 생성
    footer, header, navbar, Noticepage 파일 생성

---
+++ src/pages/notice/NoticeFormPage.js
@@ -0,0 +1,10 @@
+import '../../styles/pages/notice/NoticeFormPage.css';
+
+const NoticeFormPage = () => {
+  return (
+    <div className="NoticeFormPage">
+    </div>
+  );
+}
+
+export default NoticeFormPage;
