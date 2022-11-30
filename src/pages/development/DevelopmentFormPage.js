2b2e36679c49d68c80feb7d918ee720ac381f1b6
구현우 <guhyeon-u@guhyeon-uui-MacBookAir.local>
2022-12-01 02:21:11 +0900

    fix (DevelopmentFormPage.js): 참조 오류로 인한 파일 이름 수정
    App.js에서 참조한 파일 이름과 생성한 파일 이름이 달라 같게 수정
    DevelopmentUploadPage.js -> DevelopmentFormPage.js

---
+++ src/pages/development/DevelopmentFormPage.js
@@ -0,0 +1,11 @@
+import '../../styles/pages/development/DevelopmentFormPage.css';
+
+const DevelopmentFormPage = () => {
+  return (
+    <div className="DevelopmentFormPage">
+      DevelopmentFormPage
+    </div>
+  );
+}
+
+export default DevelopmentFormPage;
