1f48123e67961fc0d3c81903f96da3cd6c078cd4
구현우 <guhyeon-u@guhyeon-uui-MacBookAir.local>
2022-12-01 20:35:47 +0900

    feat (LoginUtils.js): 로그인 utils 함수를 관리
    Add
    - checkInput(): 입력 id를 예외처리를 하여 올바른 값을 반환
    - sendInputData(): 예외처리 성공 시 input 값을 api로 보내기 위한 함수
    - wrongId(): 아이디 형식이 예외처리 됐을 때 알림창을 출력
    
    로그인 입력 값을 예외처리하고 백서버로 통신을 할지 알림창을 띄울지 결정

---
+++ src/pages/login/utils/LoginUtils.js
@@ -0,0 +1,25 @@
+// 작성자: 구현우
+
+import ExceptionUtils from "./ExceptionUtils";
+import LoginAPI from "../api/LoginAPI";
+
+const LoginUtils = {
+    checkInput(saveIdState, inputData, setSuccessLogin) {
+        if(ExceptionUtils.checkException(inputData.id)) {
+            this.sendInputData(saveIdState, inputData, setSuccessLogin);
+            return;
+        } 
+        this.wrongId();
+        return 'wrongId';
+    },
+
+    sendInputData(saveIdState, inputData, setSuccessLogin) {
+        LoginAPI.tryLogin(saveIdState, inputData, setSuccessLogin);
+    },
+
+    wrongId() {
+        alert('아아디 형식이 올바르지 않습니다.');
+    }
+}
+
+export default LoginUtils;
