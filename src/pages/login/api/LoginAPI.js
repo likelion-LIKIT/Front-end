f90025f65c41c3728415157f5da11f1544f36692
구현우 <guhyeon-u@guhyeon-uui-MacBookAir.local>
2022-12-01 20:39:17 +0900

    feat (LoginAPI.js): 백 서버에 로그인 정보를 보냄
    Add
    - tryLogin(): 로그인을 시도하는 함수. 백서버로 통신 코드  작성 필요
    - saveId(): 아이디를 로컬에 저장하는 함수. 후에 코드 작성 필요
    
    로그인 통신 및 결과를 처리하는 함수. 후에 추가 개발 필요

---
+++ src/pages/login/api/LoginAPI.js
@@ -0,0 +1,16 @@
+// 작성자: 구현우
+
+const LoginAPI = {
+    tryLogin(saveIdState, inputData, setSuccessLogin) {
+        // axios code        
+
+        // 로그인 성공 시 saveIdSate 판별
+        // setSuccessLogin(true);
+    },
+
+    saveId() {
+
+    }
+}
+
+export default LoginAPI;
