647f3eb7f2c533ca8666d78a8571b3e4dc137489
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-03-08 02:45:01 +0900

    feat(ActivityFrame.js): 무한 카루셀 구현
    Add
    ActivityFrame.js
    - windowWidth: 현재 화면 width
    - originalData: 원본 데이터
    - addData: 양 끝에 추가할 데이터 갯수
    - showData: 보여질 데이터
    - cardWidth: card 하나의 넓이
    - currentIdx: 현재 화면 가장 왼쪽에 있는 요소의 인덱스
    - transitionStyle: 카루셀 컨테이너의 transition 스타일
    
    - makeShowData(): 원본 데이터로 부터 보여줄 데이터를 가공하는 함수
    - getCardWidth(): 카드 하나의 넓이를 지정하는 함수
    - replaceSlide(): 좌/우로 이동할 때 끝 지점에서 처음으로 돌아오는 것을 매끄럽게 처리하는 함수
    - moveLeft(): 카루셀 왼쪽 이동 함수
    - moveRight(): 카루셀 오른쪽 이동 함수
    - updateItemIdx(): 가공된 데이터에서 보이는 카드 인덱스를 추출하는 함수
    
    - useWindowSize(): 화면 렌더링 시 브라우저의 width, height를 받고, resize event handler를 추가하는 custom hooks
    
    ActivityCard
    - showCard: depth가 각각 1, 2, 3인 카루셀이 차례대로 나타나는 이벤트를 처리해주기 위한 state 변수
    
    - setOrder: 카드의 순서와 depth에 따라 classname을 설정하는 함수
    
    - useEffect(line 38): 각각 0.6, 0.8, 1초 뒤에 showCard flag를 true로 바꾸는 역할 수행
    
    무한 카루셀 구현(추후 양끝 애니메이션 이슈 해결 필요)

--- src/pages/activity/ActivityPage/components/ActivityCard.js
+++ src/pages/activity/ActivityPage/components/ActivityCard.js
@@ -1,10 +1,80 @@
-import '../styles/ActivityCard.css';
+// 작성자 : 이수화
 
-const ActivityCard = () => {
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/ActivityCard.css";
+
+const ActivityCard = (props) => {
+  const { data, idx, width, cardIdx, currentIdx } = props;
+  const navigate = useNavigate();
+  const [showCard, setShowCard] = useState({
+    depthOne: false,
+    depthTwo: false,
+    depthThree: false,
+  });
+
+  const setOrder = () => {
+    let order;
+    if (idx === currentIdx) {
+      order = "first";
+      if (showCard.depthThree) order += " showCard";
+    } else if (idx === currentIdx + 1) {
+      order = "second";
+      if (showCard.depthTwo) order += " showCard";
+    } else if (idx === currentIdx + 2) {
+      order = "third";
+      if (showCard.depthOne) order += " showCard";
+    } else if (idx === currentIdx + 3) {
+      order = "fourth";
+      if (showCard.depthTwo) order += " showCard";
+    } else if (idx === currentIdx + 4) {
+      order = "fifth";
+      if (showCard.depthThree) order += " showCard";
+    } else order = "other";
+
+    return order;
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowCard((prev) => {
+        return {
+          ...prev,
+          depthOne: true,
+        };
+      });
+    }, 600);
+
+    setTimeout(() => {
+      setShowCard((prev) => {
+        return {
+          ...prev,
+          depthTwo: true,
+        };
+      });
+    }, 800);
+
+    setTimeout(() => {
+      setShowCard((prev) => {
+        return {
+          ...prev,
+          depthThree: true,
+        };
+      });
+    }, 1000);
+  }, []);
+
   return (
-    <div className="ActivityCard">
+    <div
+      className={`${["ActivityCard", setOrder()].join(" ")}`}
+      style={{ width: `${width}px` }}
+      onClick={() => navigate(`/activity/detail/${data.id}`)}
+    >
+      {/* cardIdx : {cardIdx}<br/>
+      currentIdx : {currentIdx} <br/>
+      idx : {idx} */}
     </div>
   );
-}
+};
 
 export default ActivityCard;
--- src/pages/activity/ActivityPage/components/ActivityFrame.js
+++ src/pages/activity/ActivityPage/components/ActivityFrame.js
@@ -1,10 +1,137 @@
-import '../styles/ActivityFrame.css';
+// 작성자 : 이수화
+
+import { useState, useLayoutEffect } from "react";
+import "../styles/ActivityFrame.css";
+import ActivityCard from "./ActivityCard";
+import { activityData as data } from "../constant/activityData";
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+}
 
 const ActivityFrame = () => {
+  // 필요 변수
+
+  // 현재 화면 width
+  const [windowWidth, windowHeigt] = useWindowSize();
+  // 원본 데이터
+  const originalData = data;
+  // 원본 데이터 길이
+  const originalDataLength = originalData.length;
+  // 추가할 데이터 갯수
+  const addCnt = 4;
+  // 보여질 데이터
+  const showData = makeShowData();
+  // card 하나의 넓이
+  const cardWidth = getCardWidth();
+  // 현재 인덱스
+  const [currentIdx, setCurrentIdx] = useState(addCnt - 2);
+  // transition 설정
+  const transitionTime = 500;
+  const style = `transform ${transitionTime}ms ease 0s`;
+  const [transitionStyle, setTransitionStyle] = useState(style);
+
+  // 원본 데이터 -> 보여줄 데이터 가공
+  function makeShowData() {
+    const frontData = originalData.slice(0, addCnt);
+    const rearData = originalData.slice(-1 * addCnt);
+    return [...rearData, ...originalData, ...frontData];
+  }
+
+  // 카드 하나 넓이 지정하기
+  function getCardWidth() {
+    // 카드 하나 넓이 : 일단 윈도우 넓이의 20%로 지정
+    let scrollWidth = 17;
+    let width = parseInt((windowWidth - 140 - scrollWidth) / 5);
+    if (width < 100) width = 100; // 최소 100px로 고정
+    return width + 1;
+  }
+
+  const replaceSlide = (index) => {
+    setTimeout(() => {
+      setTransitionStyle("");
+      setCurrentIdx(index);
+    }, transitionTime);
+  };
+
+  const moveLeft = () => {
+    let nextIdx = currentIdx - 1;
+    setCurrentIdx(nextIdx);
+    if (nextIdx - addCnt < 0) {
+      console.log(currentIdx, nextIdx);
+      nextIdx += originalDataLength;
+      replaceSlide(nextIdx);
+    }
+    setTransitionStyle(style);
+  };
+
+  const moveRight = () => {
+    let nextIdx = currentIdx + 1;
+    setCurrentIdx(nextIdx);
+    if (nextIdx - addCnt >= originalDataLength - 1) {
+      console.log(currentIdx, nextIdx);
+      nextIdx -= originalDataLength;
+      replaceSlide(nextIdx);
+    }
+    setTransitionStyle(style);
+  };
+
+  const updateItemIdx = (index) => {
+    index -= addCnt;
+    if (index < 0) {
+      index += originalDataLength;
+    } else if (index >= originalDataLength) {
+      index -= originalDataLength;
+    }
+    return index;
+  };
+
   return (
     <div className="ActivityFrame">
+      {/* 아이템 전체 리스트를 감쌀 컨테이너 */}
+      <div
+        className="activityContainer"
+        style={{
+          width: `${cardWidth * showData.length}px`,
+          transform: `translateX(${-1 * cardWidth * currentIdx}px)`,
+          transition: transitionStyle,
+        }}
+      >
+        {
+          // 아이템 전체 리스트들
+          showData.map((data, idx) => {
+            const udtIdx = updateItemIdx(idx);
+            return (
+              <ActivityCard
+                key={idx}
+                idx={idx}
+                data={data}
+                width={cardWidth}
+                cardIdx={udtIdx}
+                currentIdx={currentIdx}
+                totalLen={originalDataLength}
+              />
+            );
+          })
+        }
+      </div>
+      <div className="carouoselLeftBtn" onClick={moveLeft}>
+        <img src="/images/carousel_left_button.png" alt="왼쪽 버튼" />
+      </div>
+      <div className="carouoselRightBtn" onClick={moveRight}>
+        <img src="/images/carousel_right_button.png" alt="오른쪽 버튼" />
+      </div>
     </div>
   );
-}
+};
 
 export default ActivityFrame;
