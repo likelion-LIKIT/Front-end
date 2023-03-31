ae066f2c9a5bc0cfcce86e499455bdd6dbebe30a
GHWooo <ghw9174@gmail.com>
2023-04-01 01:44:23 +0900

    feat (MyTechnologyModal): 기술 스택 수정하는 모달 구현
    UPDATE
    
    MyUtils
    - updateTechStack: 비밀번호 수정 함수를 호출하는 함수
    
    MyTechnologyModal
    - useEffect(line 18): 모달이 켜질 시 데이터 초기값으로 수정
    - useEffect(line 24): 스택 리스트 초기 원소와 수정한 리스트 원소를 비교하여 다를 시 수정 버튼 활성화
    - hoverBadgeBtn: 뱃지 버튼위에 호버 시 색 변경 결정 함수
    - clickedBadgeBtn: 뱃지 클릭 시 색 변경을 결정하고 뱃지 수정 리스트를 수정하는 함수
    - updateTechStack: 수정한 뱃지 리스트를 통신하는 함수 -> 실패 시 모달이 꺼지지 않을 로직 구현 필요
    - showTech: 스택 뱃지 전체를 출력하고 그 위에 받아온 데이터를 대조하여 영암을 줄지 않줄지 결정하여 출력하는 함수
    
    스택 뱃지 전부를 출력하고 마우스 이벤트 구현 및 통신을 위한 로직 구현

--- src/pages/my/components/MyTechnologyModal.js
+++ src/pages/my/components/MyTechnologyModal.js
@@ -1,13 +1,119 @@
 // 작성자: 구현우
 
 import Modal from "../../../common/components/Modal";
+import { Technologies } from "../constant/Technologies";
 
+import "../styles/MyPasswordModal.css";
 import "../styles/MyTechnologyModal.css";
 
-const MyTechnologyModal = ({ show, onHide, width, height }) => {
+import { useEffect, useRef, useState } from "react";
+import MyUtils from "../utils/MyUtils";
+
+const MyTechnologyModal = ({ show, onHide, width, height, setUserInfo, userInfo, tech_stack }) => {
+  const [updateTech, setUpdateTech] = useState(tech_stack);
+
+  const badgeBtnRef = useRef([]);
+  const updateBtnRef = useRef();
+
+  useEffect(() => {
+    if (!show) {
+      setUpdateTech(tech_stack);
+    }
+  }, [show]);
+
+  useEffect(() => {
+    if (
+      tech_stack.length === updateTech.length &&
+      tech_stack.sort().every((value, index) => value === updateTech.sort()[index])
+    ) {
+      updateBtnRef.current.style = "opacity: 0.4; pointer-events: none;";
+    } else {
+      updateBtnRef.current.style = "opacity: 1; pointer-events: all; cursor: pointer;";
+    }
+  }, [updateTech]);
+
+  const hoverBadgeBtn = (index, value) => {
+    badgeBtnRef.current[index].style = `opacity: ${value};`;
+  };
+
+  const clickedBadgeBtn = (badge, clicked) => {
+    if (clicked) {
+      let changeIndex = -1;
+
+      for (let i = 0; i < updateTech.length; i++) {
+        if (badge === updateTech[i]) {
+          changeIndex = i;
+          break;
+        }
+      }
+
+      let nextTech = [];
+
+      nextTech = [
+        ...updateTech.slice(0, changeIndex),
+        ...updateTech.slice(changeIndex + 1, updateTech.length),
+      ];
+
+      setUpdateTech(nextTech);
+    } else {
+      setUpdateTech([badge, ...updateTech]);
+    }
+  };
+
+  const updateTechStack = () => {
+    MyUtils.updateTechStack(updateTech, setUserInfo, userInfo);
+    // 실패시 onHide 안하는 로직 구현 필요
+    onHide();
+  };
+
+  const showTech = () => {
+    const answer = [];
+    let key = 0;
+
+    for (let badge in Technologies) {
+      const badgeInfo = Technologies[badge];
+
+      const clicked = updateTech.includes(badge);
+      const badge_index = key++;
+
+      answer.push(
+        <div key={badge_index}>
+          <img
+            src={`https://img.shields.io/badge/${badgeInfo.name}-${badgeInfo.backColor}?style=for-the-badge&logo=${badgeInfo.logo}&logoColor=${badgeInfo.color}`}
+            alt={badgeInfo.name}
+          ></img>
+          <div
+            ref={(el) => (badgeBtnRef.current[badge_index] = el)}
+            style={clicked ? { opacity: "0" } : { opacity: "0.7" }}
+            onMouseEnter={() => hoverBadgeBtn(badge_index, "0")}
+            onMouseLeave={() => hoverBadgeBtn(badge_index, clicked ? "0" : "0.7")}
+            onClick={() => clickedBadgeBtn(badge, clicked)}
+          ></div>
+        </div>
+      );
+    }
+    return answer;
+  };
+
   return (
-    <div id="MyTechnologyModal">
-      <Modal show={show} onHide={onHide} width={width} height={height}></Modal>
+    <div className={["none_MyTechnologyModal", show && "MyTechnologyModal"].join(" ")}>
+      <Modal show={show} onHide={onHide} width={width} height={height}>
+        <div id="mytechnologymodal">
+          <div id="mytechnologymodal_header">
+            <span>
+              <span>🛠️</span>
+              <span>Technology</span>
+            </span>
+            <button onClick={() => onHide()}>닫기</button>
+          </div>
+          <div id="mytechnologymodal_body">{showTech()}</div>
+          <div id="mytechnologymodal_footer">
+            <button ref={updateBtnRef} onClick={() => updateTechStack()}>
+              수정
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
